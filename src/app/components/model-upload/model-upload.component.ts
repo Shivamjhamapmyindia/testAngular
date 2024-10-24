bb3d3bf90087dc3b33cc9dfcfb665c6a1fc652e2
shivam jha <shivam.jha@mapmyindia.com>
2024-10-24 17:40:33 +0530

    first commt

---
+++ src/app/components/model-upload/model-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './model-upload.component.html',
+  styleUrl: './model-upload.component.css'
+})
+export class ModelUploadComponent {
+
+}
